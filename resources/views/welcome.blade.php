3d31ab40aed318801b61bbabd9a8d0a8842858ba
Mohamed NDIAYE <123922979+thecodecoffeelab@users.noreply.github.com>
2023-04-02 06:50:22 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mohamed NDIAYE - Open AI</title>
+    <link rel="stylesheet" href="{{ secure_asset('resources/css/app.css') }}">
     @vite('resources/css/app.css')
 </head>
 <body class="container mx-auto">
